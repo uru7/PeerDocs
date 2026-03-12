d6d6db36a6db21233be8592c635c8bd22ab01711
Uru <theururoom@gmail.com>
2026-03-13 01:19:57 +0530

    add sqlite database setup and tables

--- app.py
+++ app.py
@@ -1,10 +1,60 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session, url_for
+import sqlite3
+import os
+import random
+import string
+from datetime import datetime
 
 app = Flask(__name__)
+app.secret.key = "very_simple_secret"
 
-@app.route("/")
-def home():
-    return "PeerDocks is alive"
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def get_db():
+    return sqlite3.connect("instance/database.db")
+
+def init_db():
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   username TEXT UNIQUE,
+                   password TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS room_member (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   user_id INTEGER,
+                   room_id INTEGER
+    )
+    """)
+
+    cursor.execute(""" 
+    CREATE TABLE IF NOT EXISTS documents (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   filename TEXT,
+                   description TEXT
+                   upload_date TEXT,
+                   room_id INTEGER
+    )
+    """)
+
+    cursor.execute(""" 
+    CREATE TABLES IF NOT EXISTS documents (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   filename TEXT,
+                   description TEXT,
+                   upload_date TEXT,
+                   room_id INTEGER
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
